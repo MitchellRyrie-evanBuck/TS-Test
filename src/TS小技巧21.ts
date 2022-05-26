f471331c4279efd6b701054d8995e28b3511a10f
liuxiaowen <18264480900@163.com>
2022-05-26 16:33:54 +0800

    feat: 技巧21

---
+++ src/TS小技巧21.ts
@@ -0,0 +1,24 @@
+/*
+ * @Date: 2022-05-26 16:30:34
+ * @LastEditors: liuxiaowen
+ * @LastEditTime: 2022-05-26 16:33:18
+ * @FilePath: /TS小案例/src/TS小技巧21.ts
+ */
+const tsPeople = [
+  "Andarist",
+  "Tistalop",
+  "Angry"
+] as const
+
+// tsPeople[0] = "ensd"
+
+const moreTsPeople = {
+  Andar: "anskd",
+  oper: "sddf"
+} as const
+
+
+// moreTsPeople.Andar = "ensd"
+
+
+
