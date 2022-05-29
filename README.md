6c0e2a281a4813bb504ccb9a7179f51806ee30c3
liuxiaowen <18264480900@163.com>
2022-05-29 22:06:53 +0800

    feat: 元组转换对象

---
+++ README.md
@@ -0,0 +1,10 @@
+<!--
+ * @Date: 2022-05-29 21:55:09
+ * @LastEditors: liuxiaowen
+ * @LastEditTime: 2022-05-29 21:55:28
+ * @FilePath: /TS小案例/README.md
+-->
+# TS练习
+
+
+
--- src/TS_type_ gymnastics/03.元组转换为对象.ts
+++ src/TS_type_ gymnastics/03.元组转换为对象.ts
@@ -1,13 +1,13 @@
 /*
  * @Date: 2022-05-28 13:25:07
  * @LastEditors: liuxiaowen
- * @LastEditTime: 2022-05-28 13:31:39
+ * @LastEditTime: 2022-05-29 15:27:09
  * @FilePath: /TS小案例/src/TS_type_ gymnastics/03.元组转换为对象.ts
  */
 // 传入一个元组类型，将这个元组类型转换为对象类型，这个对象类型的键 / 值都是从元组中遍历出来。
 // 例如：
 const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
-
+type sr = typeof tuple
 type TupleToObject<T extends readonly string[]> = {
   [K in T[number]]: K
 }
